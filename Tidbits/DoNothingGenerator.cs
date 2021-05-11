9fee883a96a03dfa9300a524c65ef5102f527ce5
Jack Tyrer <jack@tyrer.dev>
2021-05-12 00:09:13 +0100

    Add Tidbits project for things not fully related to the main article

---
+++ Tidbits/DoNothingGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Generator
+{
+    /// <summary>
+    /// A generator that does nothing.
+    /// </summary>
+    [Generator]
+    public class DoNothingGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
